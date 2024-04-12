6ea1330cd40dde94baecdea5117470828960bf57
Sooyoung <agriades23@gmail.com>
2024-04-12 16:14:23 +0900

    그리드뷰용 액티비티 생성

---
+++ app/src/main/java/com/sparta/skilled/GridViewActivity.kt
@@ -0,0 +1,11 @@
+package com.sparta.skilled
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GridViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grid_view)
+    }
+}
